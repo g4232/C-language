c33f9664ae20cf942e783a708870493b7e3ad990
stork boy <1728390639@qq.com>
2022-11-05 23:35:34 +0800

    第二章练习

--- 数据结构和算法/课后练习题/main.c
+++ 数据结构和算法/课后练习题/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 typedef struct Sqlist
 {
 	int* data;
 	int length;
 }Sqlist;
 void PrintList(Sqlist* L) {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < L->length; i++)
 	{
 		printf("%d\n", L->data[i]);
 	}
@@ -18,7 +19,7 @@ void InitList(Sqlist* L) {
 	{
 		for (int i = 0; i < 10; i++)
 		{
-			L->data[i] = i;
+			L->data[i] = rand()%10+1;
 			L->length++;
 		}
 			L->data[5] = 7;
@@ -41,10 +42,66 @@ int Delete_Same(Sqlist* L) {
 	L->length = i + 1;
 	return 1;
 }
+//第二章 01 删除并返回最小的值
+int Delete_Mix(Sqlist* L) {
+	int mix = L->data[0];
+	if (L->length==0)
+	{
+		printf("该顺序表为空!!");
+	}
+	for (size_t i = 1; i < L->length; i++)
+	{
+		if (L->data[i]<mix)
+		{
+			mix = L->data[i];
+		}
+	}
+	return mix;
+
+}
+//      02 反转顺序表元素
+void Reserve_List(Sqlist* L) {
+	int i = 0, j = L->length-1;
+	for (size_t i = 0; i < j; i++)
+	{
+		int middle = 0;
+		middle = L->data[i];
+		L->data[i] = L->data[j];
+		L->data[j] = middle;
+		j--;
+	}
+
+
+}
+//      03 删除指定值的元素 O(n)时间复杂度
+void Delete_assign(int data,Sqlist* L) {
+	int flag = 0;
+	for (size_t i = 0; i < L->length; i++)
+	{
+		if (L->data[i] == data)
+		{
+			printf("找到元素 --> %d \n",L->data[i]);
+			int j = i;
+			for (int j = i; j < L->length; j++)
+			{
+				L->data[j] = L->data[j + 1];
+			}
+				L->length-=1;
+				i--;
+		}
+	}
+
+}
+
 int main() {
+	srand((unsigned)time(NULL));
 	Sqlist list;
 	InitList(&list);
-	Delete_Same(&list);
+	//Reserve_List(&list);
+	PrintList(&list);
+	list.data[3] = 2;
+	Delete_assign(2, &list);
+	printf("-------------\n");
 	PrintList(&list);
 	return 0;
 }
